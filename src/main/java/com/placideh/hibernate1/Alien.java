0ca0b089d0f94c9e45d5726b5f9069809ba07f21
Placideh <h.uwizeyeplacide@gmail.com>
2020-11-11 12:43:38 +0200

    'hibernate1'

---
+++ src/main/java/com/placideh/hibernate1/Alien.java
@@ -0,0 +1,39 @@
+package com.placideh.hibernate1;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name="alien")
+public class Alien {
+	@Id
+	private int aid;
+	//if you don't want that this name in the database we set it to transient
+	@Transient
+	private String aname;
+	@Column(name="acolor")
+	private String color;
+	
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+}
